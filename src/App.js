d74ae1e62b918e3bf2d7156524b5eaed0a39eb9b
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2018-02-15 23:47:20 -0200

    simple tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import ReactDOM from 'react-dom';
+import logo from './logo.png';
+import Game from './game/Game';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Tic Tac Toe</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, set your name and click start, 
+          <br/>then just wait for play come in and have fun.
         </p>
+        <section className="App-Settings">
+          <p>
+            <label htmlFor="name">User name: </label>
+            <input value={this.state.username} onChange={() => this.setState({name:this.value})}  id="name" />
+          </p>
+        </section>
+        <Game />
       </div>
     );
   }
