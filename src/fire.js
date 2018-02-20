76e1ddc84607b0ea2a339eb4d09494787715f496
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2018-02-19 21:34:17 -0300

    add firebase

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyBhJnLhqv6BPlo9whTgE3sRvNn75CmV-LA",
+  authDomain: "tic-tac-toe-195701.firebaseapp.com",
+  databaseURL: "https://tic-tac-toe-195701.firebaseio.com",
+  projectId: "tic-tac-toe-195701",
+  storageBucket: "tic-tac-toe-195701.appspot.com",
+  messagingSenderId: "621638425292"
+};
+firebase.initializeApp(this.config);
