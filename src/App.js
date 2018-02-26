0159b088440127b45ec5fe644059d72157c4c87c
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2018-02-26 00:34:21 -0300

    added fire Auth

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import Loadable from 'react-loadable';
 import './App.css';
+import LoadingComponent from './LoadingComponent';
+import SignInScreen from './Fire';
 
-function LoadingComponent(props) {
-  if (props.error) {
-    // When the loader has errored
-    return <div>Error!</div>;
-  } else if (props.timedOut) {
-    // When the loader has taken longer than the timeout
-    return <div>Taking a long time...</div>;
-  } else if (props.pastDelay) {
-    // When the loader has taken longer than the delay
-    return <div>Loading...</div>;
-  } else {
-    // When the loader has just started
-    return null;
-  }
-}
 const LoadableGame = Loadable({
   loader: () => import('./game/Game'),
   loading: LoadingComponent,
@@ -29,37 +16,48 @@ class App extends Component {
     super(props);
     this.state = {
       username: '',
-      usernameRW: false,
       boardSize: 3,
     };
   }
 
   handleUsername(event){
-    if(!this.state.usernameRW)
+    if(!this.state.username)
       this.setState({username: event.target.value});
   }
 
-  handleBoardSizeChange(event){
+  handleBoardSizeChange(event) {
     console.log(this, event);
     this.setState({boardSize: parseInt(event.currentTarget.value, 10)})
   }
+  handleAuthStateChange(user) {
+    console.log('auth change:',user);
+
+    // if (user&&user.auth()){
+    //   this.setState({username: user.auth().displayName});
+    // }
+  }
 
   render() {
-    return (
-      <div className="App">
+    //console.log(this, firebase.auth().currentUser.displayName);
+    return (<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Tic Tac Toe App</h1>
+        <section className="App-login">
+          <SignInScreen onAuthStateChanged={this.handleAuthStateChange.bind(this)}/>
+        </section>
         </header>
-        <p className="App-intro">
-          To get started, set your name and click start, 
-          <br/>then just wait for play come in and have fun.
-        </p>
+        <section className="App-intro">
+        <div>
+          To get started, search for a match or
+          <br/>just start one and have fun.
+        </div>
+        </section>
         <section className="App-Settings">
-          <p>
+          {/* <p>
             <label htmlFor="name">User name: </label>
             <input value={this.state.username} onChange={this.handleUsername.bind(this)} name="username" />
-          </p>
+          </p> */}
           <p>
             <label htmlFor="boardsize">Select board size: </label>
             <select name="boardsize" onChange={this.handleBoardSizeChange.bind(this)}>{
@@ -69,7 +67,7 @@ class App extends Component {
           </p>
         </section>
         <section className="App-Game">
-        <LoadableGame boardSize={this.state.boardSize}/>
+          <LoadableGame boardSize={this.state.boardSize}/>
         </section>
         <section className="App-Rules">
           <p>The rules to win this game is one of above:</p>
--- src/fire.js
+++ src/fire.js
@@ -1,4 +1,7 @@
-import firebase from 'firebase';
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 const config = {
   apiKey: "AIzaSyBhJnLhqv6BPlo9whTgE3sRvNn75CmV-LA",
@@ -8,4 +11,60 @@ const config = {
   storageBucket: "tic-tac-toe-195701.appspot.com",
   messagingSenderId: "621638425292"
 };
-firebase.initializeApp(this.config);
+
+const firebaseApp = firebase.initializeApp(config);
+
+class SignInScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    signInSuccessUrl: '/',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
+  
+  state = {
+    signedIn: false
+  };
+  
+  componentDidMount() {
+    firebaseApp.auth().onAuthStateChanged((user) => {
+      this.setState({signedIn: !!user});
+    });
+  }
+
+  render() {
+    if(this.props.onAuthStateChanged) this.props.onAuthStateChanged(firebaseApp);
+    return (
+    <div>
+        {this.state.signedIn && 
+          <div>
+            Hello {firebaseApp.auth().currentUser.displayName}. You are signed In!
+            <a className="button" onClick={() => firebaseApp.auth().signOut()}>Sign-out</a>
+          </div>
+        }
+        {!this.state.signedIn && firebaseApp &&
+          <div>
+            <p>Please sign-in:</p>
+            <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+          </div>
+        }
+    </div>
+    )
+  }
+}
+
+export default SignInScreen;
