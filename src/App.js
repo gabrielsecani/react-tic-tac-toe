f14f0204848cb4c1797e2adbb356a9b3c14f8fc9
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2018-04-20 10:08:50 -0300

    fix menu list & general  font family

--- src/App.js
+++ src/App.js
@@ -8,27 +8,33 @@ import { Link } from 'react-router-dom';
 import SignInScreen from './components/SignInScreen';
 import { firebaseAuth } from './components/Fire';
 
+function MenuList(props){
+  const routes=props.routes;
+  return (
+    <ul>
+    {routes.filter(r=>!!r.display).map((r,i)=>(
+      <li key={i.toString()} ><Link to={r.path}>{r.display}</Link>
+      {r.routes?(
+        <ul>{r.routes.map((c,j)=>(<li key={i.toString()+j.toString()}><Link to={r.path+'/'+c.path}> {c.display}</Link></li>))}</ul>
+      ): ''}
+      </li>
+    ))}
+  </ul>);
+}
+
 class AppHeader extends Component {  
   handleAuthStateChange(user) {
     // console.log('auth change:', user, firebaseAuth);
-    // this.setState({});
+    // this.setState({user});
   }
-  
+
   render() {
     return (
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Tic Tac Toe App</h1>
           <nav>
-            <ul>
-              {routes.filter(r=>!!r.display).map((r,i)=>(
-                <li><Link key={i} to={r.path}>{r.display}</Link>
-                {r.routes?(
-                  <ul>{r.routes.map(c=>(<li><Link to={r.path+'/'+c.path}> {c.display}</Link></li>))}</ul>
-                ):''}
-                </li>
-              ))}
-            </ul>
+            <MenuList routes={routes}/>
           </nav>
           <section className="App-login">
             <SignInScreen firebaseAuth={firebaseAuth} onAuthStateChanged={this.handleAuthStateChange.bind(this)}/>
@@ -38,7 +44,7 @@ class AppHeader extends Component {
   }
 }
 
-const NoAuth=()=>(<section className="App-intro">Please have a sit with login and have fun!</section>);
+const NoAuth=()=>(<section className="App-intro">Please do login so have a sit with and have fun!</section>);
 
 class App extends Component {
   render() {
