7805b5d706e6c8de2ce30278a9235e9b532a000a
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2018-05-21 14:08:34 -0300

    change to enviromant keys

--- src/components/Fire.js
+++ src/components/Fire.js
@@ -1,25 +1,13 @@
 import * as firebase from 'firebase';
 
-const prodConfig = {
-  apiKey: "AIzaSyBhJnLhqv6BPlo9whTgE3sRvNn75CmV-LA",
+const config =  {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
   authDomain: "tic-tac-toe-195701.firebaseapp.com",
   databaseURL: "https://tic-tac-toe-195701.firebaseio.com",
   projectId: "tic-tac-toe-195701",
   storageBucket: "tic-tac-toe-195701.appspot.com",
   messagingSenderId: "621638425292",
 };
-const devConfig = {
-  apiKey: "AIzaSyBhJnLhqv6BPlo9whTgE3sRvNn75CmV-LA",
-  authDomain: "tic-tac-toe-195701.firebaseapp.com",
-  databaseURL: "https://tic-tac-toe-195701.firebaseio.com",
-  projectId: "tic-tac-toe-195701",
-  storageBucket: "tic-tac-toe-195701.appspot.com",
-  messagingSenderId: "621638425292",
-};
-
-const config = process.env.NODE_ENV === 'production'
-  ? prodConfig
-  : devConfig;
 
 // const firebaseApp = firebase.initializeApp(config);
 var firebaseApp;
