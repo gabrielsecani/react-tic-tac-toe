5ed144ea5768feadd0b0ddef91cead4ed2437990
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2018-11-02 02:50:28 -0300

    management anonymouse user name

--- src/components/SignInScreen.jsx
+++ src/components/SignInScreen.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase/app';
-import 'firebase/auth';
+//import auth from 'firebase/auth';
 import UserAPI from './Game/api/UserAPI';
 
 class UserName extends Component {
@@ -47,27 +47,88 @@ class UserName extends Component {
       });
   }
 
+  componentDidMount() {
+    if (!!this.props.editUserName)
+      this.setState({ editUserName: this.props.editUserName });
+  }
+
   render() {
     if (this.state.editUserName) {
       return (
         <form onSubmit={this.closeEdit.bind(this)} >
-          <input onBlur={this.closeEdit.bind(this)} type="text" name="displayName" placeholder="Your username" pattern="[A-Za-z0-9æÆøØåÅ]{3,30}" required
+          <input onBlur={this.closeEdit.bind(this)} type="text" name="displayName" placeholder="Your name" pattern="[A-Za-z0-9æÆøØåÅ]{3,30}" required autoFocus
             onChange={(e) => this.setState({ name: e.target.value })}
             value={this.state.name} />
         </form>)
     } else {
-      return (<span onClick={() => this.openEdit()}>{this.firebaseAuth.currentUser.displayName || "(click here to change)"}</span>)
+      return (<span onClick={() => this.openEdit()}>{this.state.name || "(click here to change)"}</span>)
+    }
+  }
+}
+
+class AnonyButton extends Component {
+
+  constructor(props) {
+    super(props);
+    this.firebaseAuth = props.firebaseAuth;
+    if (props.hasOwnProperty('loginAnonymous'))
+      this.setState({ loginAnonymous: props.loginAnonymous });
+  }
+  state = {
+    name: '',
+    loginAnonymous: false
+  }
+
+  signIn() {
+    this.setState({ loginAnonymous: true });
+  }
+
+  signAnonymous(e) {
+    e.preventDefault();
+    const profile = { displayName: this.state.name };
+    this.firebaseAuth.signInAnonymouslyAndRetrieveData()
+      .then((u) => {
+        u.user.updateProfile(profile)
+          .then(() =>
+            UserAPI.setUserState({
+              name: profile.displayName
+            }))
+      })
+  }
+
+  render() {
+    if (!this.state.loginAnonymous) {
+      return <div><p>Please sign-in:</p>
+        <button className="firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised firebaseui-idp-text firebaseui-id-idp-button"
+          data-provider-id="anonymous" data-upgraded="MaterialButton" onClick={this.signIn.bind(this)}>
+          <span className="firebaseui-idp-icon-wrapper">
+            <img className="firebaseui-idp-icon" alt="anonymous login" src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/mail.svg" />
+          </span>
+          <span className="firebaseui-idp-text firebaseui-idp-text-long colorblack">Sign in Anonymous</span>
+          <span className="firebaseui-idp-text firebaseui-idp-text-short colorblack">Anonymous</span>
+        </button>
+      </div>
+    } else {
+      return <div><p>Please, tell me your name:</p>
+        <form onSubmit={this.signAnonymous.bind(this)} >
+          <p><input type="text" name="displayName" placeholder="Your user name" pattern="[A-Za-z0-9æÆøØåÅ]{3,30}" required autoFocus
+            onChange={(e) => this.setState({ name: e.target.value })}
+            value={this.state.name} /></p>
+          <button type="submit" className="firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised firebaseui-idp-text firebaseui-id-idp-button"
+            data-provider-id="anonymous" data-upgraded="MaterialButton" >
+            <span className="firebaseui-idp-icon-wrapper">
+              <img className="firebaseui-idp-icon" alt="anonymous login" src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/mail.svg" />
+            </span>
+            <span className="firebaseui-idp-text firebaseui-idp-text-long colorblack">Go! Sign In</span>
+            <span className="firebaseui-idp-text firebaseui-idp-text-short colorblack">Anonymous</span>
+          </button>
+        </form>
+      </div >
     }
   }
 }
 
 class SignInScreen extends Component {
-  constructor(props) {
-    super(props);
-    if (!this.props.firebaseApp && !this.props.firebaseAuth) {
-      throw new DOMException('SignInScreen: You need to set at least one of firebaseApp or firebaseAuth properties.');
-    }
-  }
 
   // Configure FirebaseUI.
   uiConfig = {
@@ -94,9 +155,9 @@ class SignInScreen extends Component {
         }
       }
     ],
+
     callbacks: {
       signInSuccess: (auth) => {
-        debugger;
         UserAPI.setUserState({
           name: this.firebaseAuth.currentUser.displayName,
           photoURL: this.firebaseAuth.currentUser.photoURL,
@@ -108,20 +169,21 @@ class SignInScreen extends Component {
   };
 
   state = {
-    signedIn: false
+    signedIn: false,
+    loginAnonymous: false
   };
 
-  componentDidMount() {
+  componentWillMount() {
+    if (!this.props.firebaseApp && !this.props.firebaseAuth) {
+      throw new DOMException('SignInScreen: You might set at least one of firebaseApp or firebaseAuth properties.');
+    }
     this.firebaseApp = this.props.firebaseApp;
     this.firebaseAuth = this.props.firebaseAuth || this.firebaseApp.auth();
     if (!!this.firebaseApp || this.firebaseApp !== this.firebaseAuth.app) this.firebaseApp = this.firebaseAuth.app;
+
     this.firebaseAuth.onAuthStateChanged((user) => {
       this.setState({ signedIn: !!user });
     });
-  }
-
-  signAnonymous() {
-    this.firebaseAuth.signInAnonymouslyAndRetrieveData();
   }
 
   render() {
@@ -139,15 +201,7 @@ class SignInScreen extends Component {
           </div>}
         {!this.state.signedIn && this.firebaseApp &&
           <div>
-            <p>Please sign-in:</p>
-            <button className="firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised firebaseui-idp-text firebaseui-id-idp-button"
-              data-provider-id="anonymous" data-upgraded="MaterialButton" onClick={() => this.signAnonymous()}>
-              <span className="firebaseui-idp-icon-wrapper">
-                <img className="firebaseui-idp-icon" alt="anonymous login" src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/mail.svg" />
-              </span>
-              <span className="firebaseui-idp-text firebaseui-idp-text-long colorblack">Sign in Anonymous</span>
-              <span className="firebaseui-idp-text firebaseui-idp-text-short colorblack">Anonymous</span>
-            </button>
+            <AnonyButton firebaseAuth={this.firebaseAuth} loginAnonymous={this.state.loginAnonymous} />
             <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={this.firebaseAuth} autoUpgradeAnonymousUsers="true" />
           </div>
         }
