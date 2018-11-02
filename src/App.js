db3e473ab2c19c6e80aa0ea1dcb60b934a81c733
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2018-11-02 02:35:27 -0300

    remove routes array from App

--- src/App.js
+++ src/App.js
@@ -8,43 +8,36 @@ import { Link } from 'react-router-dom';
 import SignInScreen from './components/SignInScreen';
 import { firebaseAuth } from './components/Fire';
 
-function MenuList(props) {
-  const routes = props.routes;
-  return (
-    <ul>
-      {routes.filter(r => !!r.display).map((r, i) => (
-        <li key={i.toString()} ><Link to={r.path}>{r.display}</Link>
-          {r.routes ? (
-            <ul>{r.routes.map((c, j) => (<li key={i.toString() + j.toString()}><Link to={r.path + '/' + c.path}> {c.display}</Link></li>))}</ul>
-          ) : ''}
-        </li>
-      ))}
-    </ul>);
-}
+const MenuList = () => (
+  <ul>
+    {routes.filter(r => !!r.display).map((r, i) => (
+      <li key={i.toString()} ><Link to={r.path}>{r.display}</Link>
+        {r.routes ? (
+          <ul>{r.routes.map((c, j) => (<li key={i.toString() + j.toString()}><Link to={r.path + '/' + c.path}> {c.display}</Link></li>))}</ul>
+        ) : ''}
+      </li>
+    ))}
+  </ul>
+);
 
 class AppHeader extends Component {
-  handleAuthStateChange(user) {
-    // console.log('auth change:', user, firebaseAuth);
-    // this.setState({currentUser: user});
-  }
-
   render() {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to Tic Tac Toe App</h1>
+        <h1 className="App-title">Welcome to Tic Tac Toe Game App</h1>
         <nav>
-          <MenuList routes={routes} />
+          <MenuList />
         </nav>
         <section className="App-login">
-          <SignInScreen firebaseAuth={firebaseAuth} onAuthStateChanged={this.handleAuthStateChange.bind(this)} />
+          <SignInScreen firebaseAuth={firebaseAuth} />
         </section>
       </header>
     );
   }
 }
 
-const NoAuth = () => (<section className="App-intro">Please do login then have a sit with and have fun!</section>);
+const NoAuth = () => (<section className="App-intro">Please do login then please have a sit and have fun!</section>);
 
 class App extends Component {
   render() {
